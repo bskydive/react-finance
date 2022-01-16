20921c144756df56ecdab1a206413e51dd6344d5
Valeriy Stepanov <stepanovv.ru@yandex.ru>
2022-01-16 20:31:46 +0300

    nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import './App.scss';
+import NavigationComponent from './components/Navigation/NavigationComponent';
 import RoutesComponent from './components/Routes/MainRoutesComponent';
 
 function App() {
     return (
         <div className="App">
-            <header className="App-header">
-                <p>REFIN</p>
-            </header>
+            <NavigationComponent />
 
             <BrowserRouter>
                 <RoutesComponent />
