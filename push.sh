77edf94daff84c0008b2dcc9dda4d3611bfe50c8
Valeriy Stepanov <stepanovv.ru@yandex.ru>
2022-01-17 03:45:42 +0300

    [AUTO] push all uncommited

---
+++ push.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# push it
+
+git add src
+
+untracked_files=`git ls-files -o --exclude-standard`
+untracked_count=`echo -n ${untracked_files} | wc -l`
+[[ ${untracked_count} -gt 0 ]] && echo -e "\n\n\n!!! UNTRACKED FILES: ${untracked_count} \n ${untracked_files}" && exit
+
+msg=$1
+
+[ -z ${msg} ] && git commit -am ${msg} || git commit -am "[AUTO] push all uncommited"
