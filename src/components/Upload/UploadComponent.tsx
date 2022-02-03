1d19fdd4bee2985858772e36d66dd781a6c81276
Valeriy Stepanov <stepanovv.ru@yandex.ru>
2022-02-03 18:52:25 +0300

    bootstrap, forms

--- src/components/Upload/UploadComponent.tsx
+++ src/components/Upload/UploadComponent.tsx
@@ -1,21 +1,23 @@
-import React, { Component, RefObject } from 'react';
+import React, { Component, RefObject, useState } from 'react';
+import { Stack, Container, Row, Col, Form, Button } from 'react-bootstrap';
 import './UploadComponent.scss';
 
 /**
  * https://reactjs.org/docs/refs-and-the-dom.html#creating-refs
  */
-export class UploadComponent extends Component<unknown, { path: string }> {
+export class UploadComponent extends Component<unknown, { path: string; isPathValid: boolean }> {
     private readonly inputOpenFileRef: RefObject<HTMLInputElement>;
+    // public [validated, setValidated] = useState(false);
 
     constructor(props: unknown) {
         super(props);
-        this.state = { path: 'выберите csv файл' };
+        this.state = { path: 'выберите csv файл', isPathValid: false };
         this.setPath = this.setPath.bind(this);
         this.inputOpenFileRef = React.createRef();
     }
 
     setPath(event: any): void {
-        this.setState({ path: event?.target?.files[0]?.name || '' });
+        this.setState({ ...this.state, path: event?.target?.files[0]?.name || '' });
         console.log('updated', event?.target);
     }
 
@@ -28,14 +30,54 @@ export class UploadComponent extends Component<unknown, { path: string }> {
         this.inputOpenFileRef?.current?.click();
     };
 
+    handleSubmit = (event: any) => {
+        const form = event.currentTarget;
+        if (form.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+        }
+
+        this.setState({ ...this.state, isPathValid: true });
+    };
+
     render() {
         return (
             <div className="app-upload">
-                <h3>Upload</h3>
-                <input ref={this.inputOpenFileRef} type="file" style={{ display: 'none' }} onChange={this.setPath} />
-                <label htmlFor="id_path">{this.state.path}</label>
-                <input id="id_path" type="text" value={this.state.path} onChange={this.setPath}></input>
-                <button onClick={this.showOpenFileDlg}>Parse</button>
+                <Container>
+                    <Row className="justify-content-center">
+                        <Col xs sm md lg xl xxl={4}>
+                            <Stack gap={3}>
+                                <h3>Upload</h3>
+                                <input
+                                    ref={this.inputOpenFileRef}
+                                    type="file"
+                                    style={{ display: 'none' }}
+                                    onChange={this.setPath}
+                                />
+                                <label htmlFor="id_path">{this.state.path}</label>
+                                <input id="id_path" type="text" value={this.state.path} onChange={this.setPath}></input>
+                                <button onClick={this.showOpenFileDlg}>Parse</button>
+
+                                <h3>Upload</h3>
+                                <Form noValidate validated={this.state.isPathValid} onSubmit={this.handleSubmit}>
+                                    <Form.Group className="mb-3" controlId="formPath">
+                                        <Form.Label htmlFor="formPath">File name</Form.Label>
+                                        <Form.Control
+                                            type="text"
+                                            placeholder="Enter path"
+                                            value={this.state.path}
+                                            onChange={this.setPath}
+                                        />
+                                    </Form.Group>
+
+                                    <Button variant="primary" className="mr-1" onClick={this.showOpenFileDlg}>
+                                        Parse
+                                    </Button>
+                                </Form>
+                            </Stack>
+                        </Col>
+                    </Row>
+                </Container>
             </div>
         );
     }
--- src/views/MainPageView/MainPageView.tsx
+++ src/views/MainPageView/MainPageView.tsx
@@ -1,16 +1,26 @@
 import React, { Component } from 'react';
+import {Container, Row, Col } from 'react-bootstrap';
 import { Outlet } from 'react-router-dom';
 import NavigationComponent from '../../components/Navigation/NavigationComponent';
 import './MainPageView.scss';
 
+/**
+ * https://react-bootstrap.github.io/layout/grid/
+ */
 export class MainPageView extends Component {
     render() {
         return (
             <div className="app-mainPage">
-                <div className="app-mainPage__wrapper">
-                    <NavigationComponent />
-                    <Outlet />
-                </div>
+                <Container>
+                    <Row>
+                        <Col>
+                            <div className="app-mainPage__wrapper">
+                                <NavigationComponent />
+                                <Outlet />
+                            </div>
+                        </Col>
+                    </Row>
+                </Container>
             </div>
         );
     }
